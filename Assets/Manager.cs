6680a968fe203565b1d7b804a36977b9f1112194
bumansky <brandon.m.umansky@jpl.nasa.gov>
2019-10-23 13:33:32 -0700

    created file obejcts in a circle, need to fix rotation

--- Assets/Manager.cs
+++ Assets/Manager.cs
@@ -10,9 +10,26 @@ public class Manager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        float radius = 2f;
         for(int i = 0; i < 10; i++)
         {
+            float angle = i * Mathf.PI * 2f / 8;
+            Vector3 newPos = new Vector3(0, Mathf.Cos(angle) * radius, Mathf.Sin(angle) * radius);
+            Vector3 direction = newPos + new Vector3(0, 90, 0);
+            GameObject go = Instantiate(file, newPos, Quaternion.FromToRotation(Vector3.up, newPos));
+        }
 
+        //instantiateInCircle(file, new Vector3(5, 0, 0), 10);
+    }
+
+    public void instantiateInCircle(GameObject obj, Vector3 location, int howMany)
+    {
+        float radius = howMany > 4 ? howMany / 2 : 2;
+        for (int i = 0; i < 10; i++)
+        {
+            float angle = i * Mathf.PI * 2f / 8;
+            Vector3 newPos = new Vector3(0, Mathf.Cos(angle) * radius, Mathf.Sin(angle) * radius);
+            GameObject go = Instantiate(obj, newPos, Quaternion.identity);
         }
     }
 
